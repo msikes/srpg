ca1441484b9b29b513c28b83a8098e5949ad8946
unknown <margosikes@gmail.com>
2021-06-20 18:33:39 -0500

    initial project commit

---
+++ SRPG/Assets/Scripts/NPCMove.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMove : TacticsMove
+{
+    GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward);
+
+        if (!turn)
+        {
+            return;
+        }
+
+        if (!moving)
+        {
+            FindNearestTarget();
+            CalculatePath();
+            FindSelectableTiles();
+            actualTargetTile.target = true;
+        }
+        else
+        {
+            Move();
+        }
+    }
+
+    void CalculatePath()
+    {
+        Tile targetTile = GetTargetTile(target);
+        FindPath(targetTile);
+    }
+
+    void FindNearestTarget()
+    {
+        GameObject[] targets = GameObject.FindGameObjectsWithTag("Player");
+
+        GameObject nearest = null;
+        float distance = Mathf.Infinity;
+
+        foreach (GameObject obj in targets)
+        {
+            //TODO: May be cheaper to compare distances with SqrMagnitude
+            float d = Vector3.Distance(transform.position, obj.transform.position);
+            
+            if (d < distance)
+            {
+                distance = d;
+                nearest = obj;
+            }
+        }
+
+        target = nearest;
+    }
+}
